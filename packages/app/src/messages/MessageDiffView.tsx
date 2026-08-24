09c540a95888bd46f2a859bccb06b68f362f3ba3
MrWangJustToDo <2711470541@qq.com>
2026-08-25 00:59:52 +0800

    style(app): frame edit/write diffs with toolResult halfbox
    Match rich tool outputs: MessageDiffView wraps EditDiff in
    HalfLinePaddedBox with BG.toolResult instead of the removed border.

--- packages/app/src/messages/MessageDiffView.tsx
+++ packages/app/src/messages/MessageDiffView.tsx
@@ -1,6 +1,8 @@
 import { memo } from "react";
 
+import { HalfLinePaddedBox } from "../components/HalfLinePaddedBox.js";
 import { EditDiff } from "../components/EditDiff.js";
+import { BG } from "../theme/colors.js";
 
 export type MessageDiffViewProps = {
   diffId: string;
@@ -25,15 +27,17 @@ export const MessageDiffView = memo(function MessageDiffView({
   startLine,
 }: MessageDiffViewProps) {
   return (
-    <EditDiff
-      id={diffId}
-      width={width}
-      height={height}
-      oldPath={oldPath}
-      oldFile={oldFile}
-      newPath={newPath}
-      newFile={newFile}
-      startLine={startLine}
-    />
+    <HalfLinePaddedBox backgroundColor={BG.toolResult} width={width}>
+      <EditDiff
+        id={diffId}
+        width={width}
+        height={height}
+        oldPath={oldPath}
+        oldFile={oldFile}
+        newPath={newPath}
+        newFile={newFile}
+        startLine={startLine}
+      />
+    </HalfLinePaddedBox>
   );
 });
